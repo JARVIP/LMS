32cec2ccc4b7f7a877a9486062a087934be83aa1
oleg gugunava <oleggugunava@gmail.com>
2019-02-12 11:51:51 +0400

    change

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
-﻿window.alert("aq var");
-
-
+﻿//window.alert("aq var");
 function addScript( src ) {
   var s = document.createElement( 'script' );
   s.setAttribute( 'src', src );
   document.body.appendChild( s );
 }
 
-addScript("https://drive.google.com/file/d/1pr9gQ0uViAIQjIW0Mj4mRCvxLUrsU4MO/view?usp=sharing");
+addScript("https://gitcdn.xyz/repo/JARVIP/LMS/master/extension.js");
 
-var data = store.data.items;
 
-consol.log(data);
+window.onload = function (e) {
+    var data = getData();
+    console.log(data);
+};
+
