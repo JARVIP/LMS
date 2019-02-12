f5e99b4e74c4c33779f8a89f70b8a2e3ae925dd5
Oleg Gugunava <23122213+JARVIP@users.noreply.github.com>
2019-02-12 11:32:24 +0400

    upload

---
+++ background.js
@@ -0,0 +1,14 @@
+﻿window.alert("aq var");
+
+
+function addScript( src ) {
+  var s = document.createElement( 'script' );
+  s.setAttribute( 'src', src );
+  document.body.appendChild( s );
+}
+
+addScript("https://drive.google.com/file/d/1pr9gQ0uViAIQjIW0Mj4mRCvxLUrsU4MO/view?usp=sharing");
+
+var data = store.data.items;
+
+consol.log(data);
